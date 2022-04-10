6b8402064cf60d8e230d72d4e68e7ca3f7cc4b45
Abdul Rehman <arehmannasir@outlook.com>
2022-04-10 02:36:22 +0100

    initial working code

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 import os
+import sched
 import schedule
 import time
 
@@ -12,7 +13,7 @@ class AzaanServer:
         self.host = self.parse_config()
         self.player = Player(self.host, "{}/azaans".format(os.getcwd()))
         self.setup_azaans_for_the_day()
-        self.create_daily_scheduler()
+        # self.create_daily_scheduler()
 
         while True:
             schedule.run_pending()
@@ -22,11 +23,12 @@ class AzaanServer:
         schedule.every().day.at("00:01").do(self.setup_azaans_for_the_day)
 
     def setup_azaans_for_the_day(self):
+        schedule.clear()
         day = str(time.localtime().tm_mday).zfill(2)
         month = str(time.localtime().tm_mon).zfill(2)
         year = str(time.localtime().tm_year).zfill(2)
         file_name = "{}-{}-{}.json".format(day, month, year)
-        file_name = "01-01-2022.json"
+        # file_name = "01-01-2022.json"
         with open("prayer_timings/{}".format(file_name), 'r') as infile:
             timings = json.load(infile)
             print(timings)
@@ -37,6 +39,7 @@ class AzaanServer:
             else:
                 schedule.every().day.at(timings[prayer_time]).do(self.play_azaan_fajr)
             print('scheduled {} at {}'.format(prayer_time, timings[prayer_time]))
+        self.create_daily_scheduler()
 
     def play_azaan_fajr(self):
         self.player.play(True)
